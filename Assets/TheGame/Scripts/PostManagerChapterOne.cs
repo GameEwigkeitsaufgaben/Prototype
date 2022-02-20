3e6448909b7319c98e64ccfc1c173ffb62bd06db
smeerws <hello@smeerws.at>
2022-02-20 06:16:28 +0100

    fix load overlay bug

--- Assets/TheGame/Scripts/PostManagerChapterOne.cs
+++ Assets/TheGame/Scripts/PostManagerChapterOne.cs
@@ -1,18 +1,37 @@
-//Class is resposible for post activation and pin check
+//Class is resposible for post activation
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PostManagerChapterOne : MonoBehaviour
 {
     public GameObject overlayParent;
-    
+    private List <Overlay> overlayList = new List<Overlay>();
+    private Overlay[] overlayArray;
+    private Dictionary<string, Overlay> dictOverlay= new Dictionary<string, Overlay>();
+
     private void Awake()
     {
-        if (GameData.overlayToLoad != "")
+        overlayArray = overlayParent.GetComponentsInChildren<Overlay>(true);
+
+        foreach (Overlay a in overlayArray)
         {
-            GameObject go = overlayParent.transform.Find(GameData.overlayToLoad).gameObject;
-            go.SetActive(true);
+            dictOverlay.Add(a.name, a);
+        }
+
+        if (GameData.overlayToLoad != "" && dictOverlay != null)
+        {
+            dictOverlay[GameData.overlayToLoad].gameObject.SetActive(true);
             GameData.overlayToLoad = "";
         }
     }
 
+    private void PrintAllDictKeys(string calledFrom)
+    {
+        Debug.Log("Dict length in "+ calledFrom +" "+ dictOverlay.Count);
+
+        foreach (string key in dictOverlay.Keys)
+        {
+            Debug.Log(calledFrom + ": " + key);
+        }
+    }
 }
