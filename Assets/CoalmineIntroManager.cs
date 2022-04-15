4905d913c9430c5138485f5c17f307c22d409dfd
smeerws <hello@smeerws.at>
2022-04-15 07:00:37 +0200

    clean hierarchy intro coalmine

--- Assets/CoalmineIntroManager.cs
+++ Assets/CoalmineIntroManager.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CoalmineIntroManager : MonoBehaviour
 {
     public CoalmineSpeechManger mySpeechManger;
     SwitchSceneManager switchScene;
-
+    public Button nextSceneBtn;
     // Start is called before the first frame update
     void Start()
     {
         switchScene = gameObject.GetComponent<SwitchSceneManager>();
-        mySpeechManger.playCaveIntro = true;    
+        mySpeechManger.playCaveIntro = true;
+        ColorBlock colorBlock = ColorBlock.defaultColorBlock;
+        colorBlock.normalColor = GameColors.defaultInteractionColorNormal;
+        colorBlock.highlightedColor = GameColors.defaultInteractionColorHighlighted;
+        colorBlock.pressedColor = GameColors.defaultInteractionColorPresses;
+
+        nextSceneBtn.GetComponent<Button>().colors = colorBlock;
     }
 }
