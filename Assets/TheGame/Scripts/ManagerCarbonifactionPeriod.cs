7e096e31074a0c40726028c137f39d5c275cdf3c
smeerws <hello@smeerws.at>
2022-05-21 10:26:13 +0200

    add mouse interact icon change, flip card highlight color

--- Assets/TheGame/Scripts/ManagerCarbonifactionPeriod.cs
+++ Assets/TheGame/Scripts/ManagerCarbonifactionPeriod.cs
@@ -1,22 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ManagerCarbonifactionPeriod : MonoBehaviour
 {
     private SoChapOneRuntimeData runtimeData;
+    public Button externalLink;
 
-    public void OpenTheUrl()
-    {
-
-        Application.OpenURL("https://dinosaurpictures.org/ancient-earth/#240");
-    }
-
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         runtimeData = Resources.Load<SoChapOneRuntimeData>(GameData.NameRuntimeData);
+        externalLink.colors = GameColors.GetInteractionColorBlock();
+    }
+    private void Start()
+    {
         runtimeData.cursorDefault = null;
+    }
+    
+    //Called from Btn in Scene/Inspector
+    public void OpenTheUrl()
+    {
+        Application.OpenURL("https://dinosaurpictures.org/ancient-earth/#240");
     }
 
     public void GoBackToMuseum()
