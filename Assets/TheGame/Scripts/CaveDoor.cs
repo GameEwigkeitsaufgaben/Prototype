a35726ed89f8758e5e42513bf92d4bc83cf37fd6
GruberWilfried <wilfried.gruber@htl-salzburg.ac.at>
2022-06-14 13:21:15 +0200

    Add Cave door animation

--- Assets/TheGame/Scripts/CaveDoor.cs
+++ Assets/TheGame/Scripts/CaveDoor.cs
@@ -2,66 +2,32 @@ using UnityEngine;
 
 public class CaveDoor : MonoBehaviour
 {
-    public Transform opendPosition;
-    public float speed = 0.1f;
-    public bool move = false;
-
-    public bool doorOpened = false;
-    private Vector3 targetPosition;
-    private Vector3 closedPosition;
+    public Animator anim;
     private SoSfx sfx;
     public bool playDoorSound;
     
 
     private void Start()
     {
-        closedPosition = gameObject.transform.localPosition;
-        targetPosition = closedPosition;
-        
-        gameObject.transform.localPosition = opendPosition.transform.localPosition;
-        doorOpened = true;
-
         sfx = Resources.Load<SoSfx>(GameData.NameConfigSfx);
         gameObject.GetComponent<AudioSource>().clip = sfx.coalmineCaveMoveDoors;
     }
 
     public void PlayMoveSfx()
     {
-        playDoorSound = true;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (playDoorSound && !gameObject.GetComponent<AudioSource>().isPlaying)
+        if (!gameObject.GetComponent<AudioSource>().isPlaying)
         {
             gameObject.GetComponent<AudioSource>().Play();
-            playDoorSound =  false;
-        }
-
-        //Debug.Log(gameObject.name + "is playing  " + gameObject.GetComponent<AudioSource>().isPlaying);
-
-        if (move)
-        {
-            transform.localPosition = Vector3.MoveTowards(transform.localPosition, targetPosition, speed * Time.deltaTime);
-            
-            if (transform.localPosition == targetPosition)
-            {
-                move = false;
-            }
         }
     }
+
     public void CloseDoor()
     {
-        move = true;
-        doorOpened = false;
-        targetPosition = closedPosition;
+        anim.SetBool("move", false);
     }
 
     public void OpenDoor()
     {
-        move = true;
-        doorOpened = true;
-        targetPosition = opendPosition.localPosition;
+        anim.SetBool("move",true);
     }
 }
